35379b09f93a1b22a553363e17ab3af5feb20d8b
Michael Hide <mhide@thezebra.com>
2020-06-20 23:31:30 -0500

    Reformat. Add keybinding (Ctrl-P) to toggle mode. Bump verison.

--- tampermonkey_scripts/Jira Board Presentation View.user.js
+++ tampermonkey_scripts/Jira Board Presentation View.user.js
@@ -1,76 +1,116 @@
 // ==UserScript==
 // @name         Jira Board Presentation View
 // @namespace    HideTheMonkey
-// @version      0.1
+// @version      0.2
 // @description  Add presentation checkbox to jira header
-// @author       github@hidefamily.net
+// @author       Michael Hide <github@hidefamily.net>
 // @match        *://*.atlassian.net/secure/RapidBoard.jspa*
 // @grant        GM_addStyle
 // ==/UserScript==
 
-// skip iframes
-if (window.self != window.top) {
-    return;
-}
-
-GM_addStyle('.tm-presentation {display: flex;flex-direction: row;-webkit-box-pack: center;justify-content: center;-webkit-box-align: center;align-items: center;height: 100%;position: relative;}');
+GM_addStyle(
+  '.tm-presentation {display: flex;flex-direction: row;-webkit-box-pack: center;justify-content: center;-webkit-box-align: center;align-items: center;height: 100%;position: relative;}'
+);
 GM_addStyle('.tm-presentation input {margin-right: 0.4em; margin-left: 1em}');
 
-const get = sel => document.querySelector(sel);
+const get = (sel) => document.querySelector(sel);
+const presentationCheckboxId = 'presentation-checkbox';
 
-const makeEl = (options) => {
-    let el;
-    if (options.text) {
-        el = document.createTextNode(options.text);
-    } else {
-        el = document.createElement(options.tag);
-    }
-    if (options.className) el.className = options.className;
-    if (options.tag === 'input') {
-        if (options.kind) el.type = options.kind;
-        if (options.onClick) {
-            el.onclick = options.onClick;
-        }
-    }
-    return el;
+const docEl = document.documentElement;
+const openFullscreen = () => {
+  if (docEl.requestFullscreen) {
+    docEl.requestFullscreen();
+  } else if (docEl.webkitRequestFullscreen) {
+    docEl.webkitRequestFullscreen();
+  }
+};
+const closeFullscreen = () => {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
 };
 
-function togglePresentationMode() {
-    const header = get('#ghx-header');
-    const operations = get('#ghx-operations');
-    const work = get('#ghx-work');
-    const gh = get('#gh');
-    let currentHeight = gh.clientHeight;
-    if (this.checked) {
-        header.setAttribute("style", "display:none;");
-        operations.setAttribute("style", "display:none;");
-        work.setAttribute("style", `height:${currentHeight}px; margin-top: 1em`);
-    } else {
-        currentHeight = currentHeight - header.clientHeight - operations.clientHeight;
-        header.setAttribute("style", "display:table;");
-        operations.setAttribute("style", "display:block;");
-        work.setAttribute("style", `height:${currentHeight}px; margin-top: unset;`);
+const makeEl = (options) => {
+  let el;
+  if (options.text) {
+    el = document.createTextNode(options.text);
+  } else {
+    el = document.createElement(options.tag);
+  }
+  if (options.className) el.className = options.className;
+  if (options.id) el.id = options.id;
+  if (options.tag === 'input') {
+    if (options.kind) el.type = options.kind;
+    if (options.onClick) {
+      el.onclick = options.onClick;
     }
-    toggleHelpPanel(this.checked);
+  }
+  return el;
+};
+
+const togglePresentationMode = (event) => {
+  const isChecked = event.currentTarget && event.currentTarget.checked;
+  const header = get('#ghx-header');
+  const operations = get('#ghx-operations');
+  const work = get('#ghx-work');
+  const gh = get('#gh');
+  let currentHeight = gh.clientHeight;
+  if (isChecked) {
+    openFullscreen();
+    header.setAttribute('style', 'display:none;');
+    operations.setAttribute('style', 'display:none;');
+    work.setAttribute('style', `height:${currentHeight}px; margin-top: 1em`);
+  } else {
+    closeFullscreen();
+    currentHeight =
+      currentHeight - header.clientHeight - operations.clientHeight;
+    header.setAttribute('style', 'display:table;');
+    operations.setAttribute('style', 'display:block;');
+    work.setAttribute('style', `height:${currentHeight}px; margin-top: unset;`);
+  }
+  toggleHelpPanel(isChecked);
 };
 
 const toggleHelpPanel = (checked) => {
-    const toggleButton = get('#navigation-app button[class~=ak-navigation-resize-button]');
-    if (toggleButton.ariaExpanded === 'true' && checked) {
-        toggleButton.click();
-    }
+  const toggleButton = get(
+    '#navigation-app button[class~=ak-navigation-resize-button]'
+  );
+  if (toggleButton.ariaExpanded === 'true' && checked) {
+    toggleButton.click();
+  }
 };
 
 const addCheckbox = () => {
-    const actionsDiv = get('header div[data-testid="atlassian-navigation--primary-actions"]');
-    const outerDiv = makeEl({tag: 'div', className: 'tm-presentation'});
-    const checkBox = makeEl({tag: 'input', kind: 'checkbox', onClick: togglePresentationMode});
-    const textNode = makeEl({text: 'Presentation Mode'});
-    outerDiv.append(checkBox);
-    outerDiv.append(textNode);
-    actionsDiv.insertBefore(outerDiv, actionsDiv.lastElementChild);
-}
+  const actionsDiv = get(
+    'header div[data-testid="atlassian-navigation--primary-actions"]'
+  );
+  const outerDiv = makeEl({ tag: 'div', className: 'tm-presentation' });
+  const checkBox = makeEl({
+    tag: 'input',
+    kind: 'checkbox',
+    id: presentationCheckboxId,
+    onClick: togglePresentationMode,
+  });
+  const textNode = makeEl({ text: 'Presentation Mode' });
+  outerDiv.append(checkBox);
+  outerDiv.append(textNode);
+  actionsDiv.insertBefore(outerDiv, actionsDiv.lastElementChild);
+};
 
 window.addEventListener('load', (event) => {
-    addCheckbox();
+  addCheckbox();
+  // Add a key binding for Ctrl-P to toggle the mode
+  window.addEventListener('keydown', (e) => {
+    if (
+      e.keyCode == 80 &&
+      e.ctrlKey &&
+      !e.shiftKey &&
+      !e.altKey &&
+      !e.metaKey
+    ) {
+      get(`#${presentationCheckboxId}`).click();
+    }
+  });
 });
